ddd1a7b7e08ed1db7a775a182571a974cf694e1d
HarleeMade <harleejackson240@gmail.com>
2026-03-14 21:24:03 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import AxiomSystems from "./AxiomSystems.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AxiomSystems />
+  </StrictMode>
+);
